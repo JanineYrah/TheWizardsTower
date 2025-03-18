1ebbfb4c5fac2440f4ca2c0e56ca030464bfd613
JanineYrah <b2027jytolentino@pshs.edu.ph>
2025-03-18 22:05:52 +0800

    Update TileManager.java

--- TheWizardsTower/src/thewizardstower/TileManager.java
+++ TheWizardsTower/src/thewizardstower/TileManager.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package tile;
+package thewizardstower;
 
 import java.awt.Graphics2D;
 import java.io.BufferedReader;
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import javax.imageio.ImageIO;
-import q3aa3_tau_alcayde.tolentino.gui.PlayingScreen;
 
 /**
  *
