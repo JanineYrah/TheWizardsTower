a76ef9e00da9cf71e16ce626636cb6ea1c19249d
JanineYrah <b2027jytolentino@pshs.edu.ph>
2025-03-18 22:08:22 +0800

    Update GameScreen.java

--- TheWizardsTower/src/thewizardstower/GameScreen.java
+++ TheWizardsTower/src/thewizardstower/GameScreen.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package thewizardstower;
+
+import thewizardstower.controller
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.MouseAdapter;
