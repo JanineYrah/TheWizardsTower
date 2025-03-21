e27b8801d5ebc6aa0d48371584d6e85ec7342aca
JanineYrah <b2027jytolentino@pshs.edu.ph>
2025-03-21 13:39:35 +0800

    Update LevelSelection.java

--- TheWizardsTower/src/thewizardstower/LevelSelection.java
+++ TheWizardsTower/src/thewizardstower/LevelSelection.java
@@ -1,102 +1,40 @@
-package thewizardstower;
+package thewizardstower.controller;
 
-import thewizardstower.controller.*;
-import java.awt.*;
-import java.util.ArrayList;
+import java.awt.event.*;
 import javax.swing.*;
+import java.util.*;
+import thewizardstower.*;
 
-public class LevelSelection extends JFrame {
-    
-    private JPanel container, levelImagePanel, levelNamePanel, levelStartPanel, levelDescriptionPanel;
-    private LevelSelectionController controller;
+public class LevelSelectionController implements ActionListener {
+    private JFrame levelSelection;
     private JLabel levelImage, levelName, levelDescription;
     private JButton previous, next, levelStart, back;
 
-    public LevelSelection() {
-        /* DISPLAY */
-        super("Select a level");
-        this.setSize(1080, 720);
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        
-        /* setting up container */
-        container = new JPanel(new FlowLayout());
-        container.setSize(1000, 720);
-        this.getContentPane().add(container);
-        
-        /* setting up panels and positions */
-        levelImagePanel = new JPanel(new FlowLayout());
-        levelImagePanel.setPreferredSize(new Dimension(1000, 700));
-        container.add(levelImagePanel);
-        
-        levelNamePanel = new JPanel(new FlowLayout());
-        levelNamePanel.setPreferredSize(new Dimension(400, 50));
-        container.add(levelNamePanel);
-        
-        levelStartPanel = new JPanel(new FlowLayout());
-        levelStartPanel.setPreferredSize(new Dimension(300, 50));
-        container.add(levelStartPanel);
-        
-        levelDescriptionPanel = new JPanel(new FlowLayout());
-        levelDescriptionPanel.setPreferredSize(new Dimension(700, 200));
-        container.add(levelDescriptionPanel);
-        
-
-        /* setting up level IMAGE object/s */
-        levelImage = new JLabel();
-        
-        levelImagePanel.add(levelImage);
-        
-        
-        
-        
-        /* setting up leve NAME object/s */
-        levelName = new JLabel();
-        
-        levelNamePanel.add(levelImage);
-        
-        
-        /* setting up level START object/s */
-        levelStart = new JButton();
-        
-        levelStartPanel.add(levelImage);
-        
-        
-        /* setting up level DESCRIPTION object/s */
-        levelDescription = new JLabel();
-        
-        levelDescriptionPanel.add(levelImage);
-
-        
-        
-        
-        /* setting up level SWITCHING object/s (next and prev) */
-        previous = new JButton("Prev");
-        previous.setPreferredSize(new Dimension(100, 40));
-        this.add(previous);
-        
-        next = new JButton("Next");
-        next.setPreferredSize(new Dimension(100, 40));
-        this.add(next);
-
-        /* setting up back object */
-        back = new JButton("Back");
-        back.setPreferredSize(new Dimension(100, 40));
-        this.add(back);
-
-        
-        
-        
-        
-        /* CONTROLLER STARTS HERE */
-        controller = new LevelSelectionController(this, levelImage, levelName, levelDescription, previous, next, levelStart, back);
-        previous.addActionListener(controller);
-        next.addActionListener(controller);
-        levelStart.addActionListener(controller);
-        back.addActionListener(controller);
-
-        /* for visibility */
-        this.setResizable(false);
-        this.setVisible(true);
+    public LevelSelectionController(JFrame levelSelection, JLabel levelImage, JLabel levelName, JLabel levelDescription,
+            JButton previous, JButton next, JButton levelStart, JButton back) {
+        this.levelSelection = levelSelection;
+        this.levelImage = levelImage;
+        this.levelName = levelName;
+        this.levelDescription = levelDescription;
+        this.previous = previous;
+        this.next = next;
+        this.levelStart = levelStart;
+        this.back = back;
     }
     
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == next) {
+        }
+        if (e.getSource() == previous) {
+        }
+        if (e.getSource() == levelStart) {
+        }
+        if (e.getSource() == back) {
+            JFrame mainMenu = new MainMenu();
+            levelSelection.dispose();
+            mainMenu.setVisible(true);  
+        }
+    } 
 }
