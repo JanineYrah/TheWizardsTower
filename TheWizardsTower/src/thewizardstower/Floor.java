db12b5c7b3a91dfbe40797a207e72572bd07e3d3
JanineYrah <b2027jytolentino@pshs.edu.ph>
2025-03-21 08:13:14 +0800

    Update Floor.java

--- TheWizardsTower/src/thewizardstower/Floor.java
+++ TheWizardsTower/src/thewizardstower/Floor.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
- * This class represents the player character class.
+ * This class represents the floor class.
  * @author Yrah
  */
 public class Floor {
