33be03d9d9412cc22f4145c4b5400a477dc17621
JanineYrah <b2027jytolentino@pshs.edu.ph>
2025-03-21 13:47:40 +0800

    Update LevelSelectionController.java

--- TheWizardsTower/src/thewizardstower/controller/LevelSelectionController.java
+++ TheWizardsTower/src/thewizardstower/controller/LevelSelectionController.java
@@ -3,7 +3,38 @@ package thewizardstower.controller;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
+import thewizardstower.*;
 
-public class LevelSelectionController {
+public class LevelSelectionController implements ActionListener {
+    private JFrame levelSelection;
+    private JLabel levelImage, levelName, levelDescription;
+    private JButton previous, next, levelStart, back;
+
+    public LevelSelectionController(JFrame levelSelection, JLabel levelImage, JLabel levelName, JLabel levelDescription,
+            JButton previous, JButton next, JButton levelStart, JButton back) {
+        this.levelSelection = levelSelection;
+        this.levelImage = levelImage;
+        this.levelName = levelName;
+        this.levelDescription = levelDescription;
+        this.previous = previous;
+        this.next = next;
+        this.levelStart = levelStart;
+        this.back = back;
+    }
     
+    
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == next) {
+        }
+        if (e.getSource() == previous) {
+        }
+        if (e.getSource() == levelStart) {
+        }
+        if (e.getSource() == back) {
+            JFrame mainMenu = new MainMenu();
+            levelSelection.dispose();
+            mainMenu.setVisible(true);  
+        }
+    } 
 }
